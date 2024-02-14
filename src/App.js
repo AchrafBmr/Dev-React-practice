cfcf34876557289b33433dfcee5ba88f2ecf7f90
achrafbmr <dawddawd555@gmail.com>
2024-02-14 21:53:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import C1 from './components/C1'
+import C2 from './components/C2'
 import C3 from './components/C3'
 
 function App() {
   return (
     <div>
       <C3/>
+      <C2/>
+      <C1/>
     </div>
   )
 }
